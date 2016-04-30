d5ab7c4c4fd79249c77a5e06b315f15666c75758
aocsa <aocsa@MacBook-Pro-de-Alexander.local>
2016-04-29 22:56:16 -0500

    commit bootstrap   Learning.Badr
    commit bootstrap   Learning.Badr

--- MLearning/BadrIndex/Bootstrap/JsonPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace MLearning.UnifiedTouch.Bootstrap
-{
-    public class JsonPluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Json.PluginLoader>
-    {
-    }
-}
